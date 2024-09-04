433a79da393fd3268c13a90051d315e2d82836a3
Rafael Mortel Gayahan <74213299+Rafael-Gayahan@users.noreply.github.com>
2024-09-03 20:01:29 -0500

    some changes

---
+++ transcriptiontool/scripts/audio-downloader.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VIDEO_ID=$1
+
+[ -z "$VIDEO_ID" ] && echo "ERROR: No video ID specified" && exit 1
+
+#echo $VIDEO_ID
+yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
